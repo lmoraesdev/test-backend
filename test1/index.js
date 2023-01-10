094d094477992d5e350f99512d6aa392b4ca32df
lmoraesdev <lbc_moraes@outlook.com>
2023-01-10 14:59:21 -0300

    parando o ambiente de desenvolvimento

---
+++ test1/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`${titulo} passou`);
+  } else {
+    console.error(`${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+
+teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
